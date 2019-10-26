823b5face8ea4c019097a60f59a648a9a18763d3
hyeon62 <hyeon.rot@gmail.com>
2019-10-26 22:06:46 +0900

    Add temporary rest api

--- api/urls.py
+++ api/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
+
+import video.api
+
+router = routers.DefaultRouter()
+router.register('videos', video.api.VideoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/doc', get_swagger_view(title='Rest API Document')),
+    url(r'^api/v1/', include((router.urls, 'video'), namespace='api')),
 ]
---
+++ video/api.py
@@ -0,0 +1,12 @@
+from .models import Video
+from rest_framework import serializers, viewsets
+
+class VideoSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = Video
+    fields = '__all__'
+
+class VideoViewSet(viewsets.ModelViewSet):
+  queryset = Video.objects.all()
+  serializer_class = VideoSerializer
