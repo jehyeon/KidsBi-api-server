eda2c8292e5e8390679979a7dd52bcc5b0b8f219
hyeon62 <hyeon.rot@gmail.com>
2019-10-26 21:51:43 +0900

    Update video model and migrate

--- video/models.py
+++ video/models.py
@@ -3,13 +3,15 @@ from django.db import models
 class Video(models.Model):
   title = models.CharField(max_length=255)
   source = models.CharField(max_length=255)
-  categories = models.ListCharField(
-    base_field = models.CharField(max_length=10)
-  )
+  # categories = models.ListCharField(
+  #   base_field = models.CharField(max_length=10)
+  # )
+  categories = models.CharField(max_length=255)
   videoId = models.CharField(max_length=50)
   videoThumbnail = models.CharField(max_length=255)
   videoFile = models.CharField(max_length=255)
-  like = models.DecimalField()
+  # like = models.DecimalField()
+  likeRatio = models.IntegerField()
   viewCount = models.IntegerField(default=0)
   callCount = models.IntegerField(default=0)
   funSticker = models.IntegerField(default=0)
