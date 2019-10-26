727a0f685818e8c80c91b417f569111cd63d0b69
hyeon62 <hyeon.rot@gmail.com>
2019-10-26 21:48:29 +0900

    Update video model field

--- video/models.py
+++ video/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+  title = models.CharField(max_length=255)
+  source = models.CharField(max_length=255)
+  categories = models.ListCharField(
+    base_field = models.CharField(max_length=10)
+  )
+  videoId = models.CharField(max_length=50)
+  videoThumbnail = models.CharField(max_length=255)
+  videoFile = models.CharField(max_length=255)
+  like = models.DecimalField()
+  viewCount = models.IntegerField(default=0)
+  callCount = models.IntegerField(default=0)
+  funSticker = models.IntegerField(default=0)
+  notFunSticker = models.IntegerField(default=0)
+  updateDate = models.DateTimeField()
